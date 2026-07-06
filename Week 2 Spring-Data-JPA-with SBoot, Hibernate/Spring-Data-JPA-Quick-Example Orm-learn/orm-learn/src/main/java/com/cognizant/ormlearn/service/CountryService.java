49f529fa246df90620718cb904d80814cf9adccb
Anbu Kumar <anbukumargv@gmail.com>
2026-07-06 12:07:18 +0530

    Add files via upload

---
+++ Week 2 Spring-Data-JPA-with SBoot, Hibernate/Spring-Data-JPA-Quick-Example Orm-learn/orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,24 @@
+package com.cognizant.ormlearn.service;
+
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
